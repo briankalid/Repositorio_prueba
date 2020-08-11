591337aedb3f033338b5289b3c7cb3432e383867
briankalid <briankalid2000@gmail.com>
2020-08-11 18:55:31 -0500

    Add password files

---
+++ Proyecto1.0/secret.py
@@ -0,0 +1,18 @@
+def consumer_key():
+    return("") #Your consumer key
+def consumer_secret():
+    return("") #Your consumer secret key
+def access_token():
+    return("") #Your access Token
+def access_secret():
+    return("") #Your access secret key
+def host_db():
+    return("") #Host of your database
+def user_db():
+    return("") #User of your database
+def password_db():
+    return ("") #Password of your database
+def name_db():
+    return ("") #Name od your databse
+def app_secret_key():
+    return('') #Secret key for your web app
